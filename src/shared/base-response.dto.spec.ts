a0c390340712053d47064b88dd13da7e76a8fe84
Gabriel <gabbygabbylexy@gmail.com>
2019-08-30 00:55:42 +0100

    added circleci config and fixed test errors

--- src/shared/base-response.dto.spec.ts
+++ src/shared/base-response.dto.spec.ts
@@ -2,6 +2,6 @@ import { BaseResponseDto } from './base-response.dto';
 
 describe('BaseResponseDto', () => {
   it('should be defined', () => {
-    expect(new BaseResponseDto()).toBeDefined();
+    expect(new BaseResponseDto('00', 'success', {})).toBeDefined();
   });
 });
