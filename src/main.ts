9ca975a067410a5e1eb8e524fa6bbe073f67b680
Gabriel <gabbygabbylexy@gmail.com>
2019-09-01 12:24:16 +0100

    created .env file and set heroku varriables for remote database connection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
+  Logger.log(`Nest application started at port ${process.env.PORT}`, 'bootstrap');
 }
 bootstrap();
